4745daa39f2ffcc06b314e98f95614f7b339baba
phuynh101 <huynhhuuphuc06072gmail.com>
2017-11-09 16:34:50 -0800

    4

--- app/src/main/java/edu/orangecoastcollege/cs273/dtallcott/connect/MainActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/dtallcott/connect/MainActivity.java
@@ -1,7 +1,11 @@
 package edu.orangecoastcollege.cs273.dtallcott.connect;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Resources res = getResources();
+        switch(item.getItemId())
+        {
+            case R.id.search:
+                Intent searchIntent = new Intent(this, SearchActivity.class);
+                startActivity(searchIntent);
+            case R.id.info:
+                Intent infoIntent = new Intent(this, InformationActivity.class);
+                startActivity(infoIntent);
+
+        }
+        Intent searchIntent = new Intent(this, SearchActivity.class);
+        startActivity(searchIntent);
+        return super.onOptionsItemSelected(item);
     }
 }
