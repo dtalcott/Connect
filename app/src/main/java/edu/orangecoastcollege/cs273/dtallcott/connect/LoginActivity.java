399767f678e59c900c1e348de68ae4dcfe4b5d08
Phuynh101 <huynhhuuphuc0607@gmail.com>
2017-11-28 17:45:41 -0800

    Modifying UserDatabase

--- app/src/main/java/edu/orangecoastcollege/cs273/dtallcott/connect/LoginActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/dtallcott/connect/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 
 /**
  * Created by devontallcott on 11/9/17.
@@ -11,15 +12,46 @@ import android.view.View;
 
 public class LoginActivity extends AppCompatActivity
 {
+    private DBHelper mDBHelper;
+    private EditText userNameEditText;
+    private EditText passwordEitText;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        userNameEditText = (EditText) findViewById(R.id.userNameEditText);
+        passwordEitText = (EditText) findViewById(R.id.passwordEitText);
+        
+        mDBHelper = new DBHelper(this);
+        
+        populateUserDatabase();
     }
 
+    public void populateUserDatabase()
+    {
+//        mDBHelper.deleteAllUsers();
+//        mDBHelper.importUsersFromCSV("users.csv");
+    }
+    
     public void gotoMainActivity(View view)
     {
+//        String username = userNameEditText.getText().toString();
+//        String password = passwordEitText.getText().toString();
+//        if(username.isEmpty() || password.isEmpty())
+//            Toast.makeText(this, R.string.empty_username_password, Toast.LENGTH_SHORT).show();
+//        else{
+//            Student student = mDBHelper.getStudentFromUser(username,password);
+//            if(student == null)
+//                Toast.makeText(this, R.string.wrong_username_or_password, Toast.LENGTH_SHORT).show();
+//            else {
+//                Intent mainIntent = new Intent(this, MainActivity.class);
+//                mainIntent.putExtra("Student",student);
+//                startActivity(mainIntent);
+//            }
+//        }
         Intent mainIntent = new Intent(this, MainActivity.class);
         startActivity(mainIntent);
     }
